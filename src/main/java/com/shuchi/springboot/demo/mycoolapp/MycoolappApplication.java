ec4fde44ac7ad2a6dceb2de2f6f7166edb725122
Shuchi Sinha <shuchi.skills19@gmail.com>
2023-10-19 17:29:30 -0700

    parameterized API path @PathVariable

--- src/main/java/com/shuchi/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/shuchi/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,14 +3,15 @@ package com.shuchi.springboot.demo.mycoolapp;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MycoolappApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(MycoolappApplication.class);
 
@@ -24,7 +25,6 @@ public class MycoolappApplication implements CommandLineRunner {
 		logger.info("Application starting " + SimpleDateFormat.getDateInstance().format(new Date()));
 		// new SimpleDateFormat("yyyy-mm-dd-hh-mm-ss").format(new Date());
 		System.setProperty("https.protocols", "TLSv1, TLSv1.1, TLSv1.2");
-	
 
 	}
 
--- src/main/java/com/shuchi/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/shuchi/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,6 +1,7 @@
 package com.shuchi.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,12 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!";
+    }
+
+    @GetMapping(value = "/{name}")
+    public String greet(@PathVariable("name") String name) {
+        var greet = "Hi %s";
+        return String.format(greet, name);
     }
 
     // expose a new endpoint for "fortune"
