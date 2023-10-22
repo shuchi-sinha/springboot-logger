d668a77a1d7f06f52ccfb17b4eb507a30b759ee9
Shuchi Sinha <shuchi.skills19@gmail.com>
2023-10-22 14:09:55 -0700

    organized imports

--- src/main/java/com/shuchi/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/shuchi/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,9 +1,8 @@
 package com.shuchi.springboot.demo.mycoolapp;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
